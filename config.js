0c50e14a735fb9918b3f71e1c28d1ff5eea14345
magenama <magenama197@gmail.com>
2024-10-29 13:26:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "U6tWSAiI#Y1RP6PhV6j1UBmms6INm3_Z_1cmJlw2Zw9gJ_7NwQhM", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeen poorna*",
+ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am Kawee Botz*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
 MODE: process.env.MODE || "public", //private or public
 AUTO_VOICE:"true" //true or false
